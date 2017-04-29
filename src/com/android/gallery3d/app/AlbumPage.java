37cdf5e1f91b2b97b534512b775543faeb03f2bc
Ahmed Hady <ahmedhady6@gmail.com>
2017-04-29 03:42:09 +0200

    snapdragongallery: bring back settings
    start with slideshow settings
    
    @mydongistiny: fixed up for use with SnapdragonGallery
    
    Change-Id: Id04a78653f10a991067a56984a963c124020eb7b

--- src/com/android/gallery3d/app/AlbumPage.java
+++ src/com/android/gallery3d/app/AlbumPage.java
@@ -712,7 +712,8 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
                 Bundle data = new Bundle();
                 data.putString(SlideshowPage.KEY_SET_PATH,
                         mMediaSetPath.toString());
-                data.putBoolean(SlideshowPage.KEY_REPEAT, true);
+                data.putBoolean(SlideshowPage.KEY_REPEAT, GalleryUtils.isRepeatSlideshow(mActivity));
+                data.putBoolean(SlideshowPage.KEY_RANDOM_ORDER, GalleryUtils.isRandomSlideshow(mActivity));
                 mActivity.getStateManager().startStateForResult(
                         SlideshowPage.class, REQUEST_SLIDESHOW, data);
                 return true;
@@ -723,6 +724,10 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
                 } else {
                     showDetails();
                 }
+                return true;
+            }
+            case R.id.action_settings: {
+                mActivity.startActivity(new Intent(mActivity, GallerySettings.class));
                 return true;
             }
             case R.id.action_camera: {
